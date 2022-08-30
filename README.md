a09883b4fd41d1154700fe0fda2fe869f1fa7706
Bruno <bruno.xti@outlook.com>
2022-08-30 16:44:02 -0300

    add implementationtimestamp

---
+++ README.md
@@ -0,0 +1,11 @@
+# Aulas Blockchain 
+
+
+Este programa é uma variação do carbuilder, o programa que permite solicitar a construção de um veículo, a sua consttrução e criação do seguro
+O passo a passo para executtar está neste vídeo abaixo
+É um programa blockchain, e depois de executado fica com esta topologia
+
+<img alt="Topologia do programa" src="topologia.png">
+
+
+[![Watch the video](video.png)(https://youtu.be/WvyLn2Yol7c)
---
+++ insurance/static/controller.js
@@ -0,0 +1,147 @@
+var app = angular.module('nebula', []);
+
+app.controller('control', [ '$scope', '$http', '$interval', '$window', function($scope, $http, $interval, $window) {
+	
+	    $interval( function(){ $scope.checkVeiculoReady(); }, 5000);
+		$interval( function(){ $scope.checkListReady(); }, 5000);
+	    const myArrayPretty = new Array();
+	    $scope.key;
+	    $scope.solicitado = false;
+	    $scope.showdialog = true;
+		$scope.mensagem = "Solicitação não enviada";
+		$scope.chassicriado_status = "list-group-item-danger";
+		$scope.pinturacriada_status = "list-group-item-danger";
+		$scope.interiorcriado_status = "list-group-item-danger";
+		$scope.veiculopronto_status = "divider-warning";
+
+		//list-group-item-success  list-group-item-danger
+
+	    
+	    $scope.blockchain_ip = "http://127.0.0.1:3546";	    
+    	
+	    
+		$scope.checkVeiculoReady = function() {
+		    //TODO CHAMA PERIODICAMENTE A URL RETRIEVE DO BLOCKCHAIN E TESTA SE O VEICULO FOI CONSTRUIDO
+			//TODO Se o valor construido for alterado para true, apresenta o botão solicita seguro
+
+			$http.get($scope.blockchain_ip + '/getQueryResultString?chave={\"selector\":{\"construcao_solicitada\":{\"$eq\":true}}}').then(function(response) {
+				let myArray = response.data.split('-');
+
+				try{
+				myArray.forEach(element => {
+					
+					if(element){
+					let x = JSON.parse(element.substring(element.indexOf("|") + 1))
+					console.log(x)
+					
+					myArrayPretty.push(x)
+					}
+					
+				});
+			}catch(err){
+				
+				console.log(err)
+			}
+			
+				const obj = myArrayPretty[0];
+				console.log(obj.chave)
+				 if (obj.chave != undefined){
+					
+					 $http.get($scope.blockchain_ip + '/retrieve?chave='+obj.chave).then(function(response) {
+						 //if (response.data.chave != undefined)
+						 $scope.bloco = response.data;
+						 console.log(response.data)
+					 });
+				 }
+			 });
+
+			$http.get($scope.blockchain_ip + '/retrieve?chave='+$scope.bloco.chave).then(function(response) {
+				if (response.data.chave != undefined)
+				$scope.bloco = response.data;
+		    });
+			// valida se o processo de consttrução está em andamento e atualiza campos na tela
+			if ($scope.bloco.chassicriado)
+				$scope.chassicriado_status = "list-group-item-success";
+			else
+				$scope.chassicriado_status = "list-group-item-danger";
+			
+			if ($scope.bloco.pinturacriada)
+				$scope.pinturacriada_status = "list-group-item-success";
+			else
+				$scope.pinturacriada_status = "list-group-item-danger";
+
+			if ($scope.bloco.interiorcriado)
+				$scope.interiorcriado_status = "list-group-item-success";
+			else
+				$scope.interiorcriado_status = "list-group-item-danger";
+
+			if ($scope.bloco.veiculopronto)
+				$scope.veiculopronto_status = "list-group-item-success";
+			else
+				$scope.veiculopronto_status = "list-group-item-danger";
+
+			if ($scope.bloco.interiorcriado && $scope.bloco.pinturacriada && $scope.bloco.chassicriado) {
+				$scope.bloco.veiculopronto = true;
+				$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+				});
+			}
+			else {
+				$scope.veiculopronto = "divider-warning";
+			}
+    	}  	 
+		
+    	$scope.modelo = function(modelo) {
+		    $scope.bloco.modelo = modelo;
+    	} 
+		
+		$scope.cor = function(cor) {
+		    $scope.bloco.cor = cor;
+    	}   
+
+		$scope.interior = function(interior) {
+		    $scope.bloco.interior = interior;
+    	}
+
+    	$scope.createCar = function() {
+			
+			$scope.bloco.construcao_solicitada = true;
+			$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+			});
+			//TODO Inicia o processo de construção do veículo
+			//TODO cria threads paa simular o preenchimento dos campos 
+
+			setTimeout(()=>{ $scope.bloco.chassicriado = true; 
+				$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+				});
+			},10000 + Math.floor(Math.random() * 25000));
+			setTimeout(()=>{ $scope.bloco.pinturacriada = true; 
+				$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+				});
+			}, 10000 + Math.floor(Math.random() * 25000));
+			setTimeout(()=>{$scope.bloco.interiorcriado = true; 
+				$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+				});
+			}, 10000 + Math.floor(Math.random() * 25000));
+
+    	}
+    	
+
+		
+    	$scope.ativaSeguro = function() {
+			$scope.bloco.segurado = true;
+			$http.get($scope.blockchain_ip + '/update?chave='+$scope.bloco.chave+"&valor="+JSON.stringify($scope.bloco)).then(function(response) {
+			});
+    	}
+
+    	$scope.solicitaSeguro = function() {
+			
+			$scope.mensagem = 'Solicitação para o seguro enviada';
+    	}
+		
+    	$scope.hasFinished = function() {
+			if ($scope.bloco.construido)
+				$scope.mensagem = 'Seu veículo foi construido com sucesso';  
+			return $scope.bloco.construido;
+    	}
+		
+   }]);
